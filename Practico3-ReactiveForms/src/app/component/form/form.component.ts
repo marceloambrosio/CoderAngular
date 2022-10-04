8b14e28c118766e0d8a03132c360af86f863a6f4
marceloambrosio <marceambrosio.ok@gmail.com>
2022-10-04 13:41:16 -0300

    practico 3

---
+++ Practico3-ReactiveForms/src/app/component/form/form.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+registroPostulante: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.registroPostulante = fb.group({
+      nombre: new FormControl('', [Validators.required]),
+      apellido: new FormControl('', [Validators.required]),
+      edad: new FormControl('', [Validators.min(25), Validators.required]),
+      correo: new FormControl('', [Validators.pattern('^[a-z]+@[a-z]+\\.[a-z]{2,3}$'), Validators.required]),
+      //titulo: new FormControl('', [Validators.required]),
+
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  agregarPostulante(){
+    console.log(this.registroPostulante);
+  }
+
+}
